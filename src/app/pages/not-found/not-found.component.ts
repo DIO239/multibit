ae323962348bc757e74152122195d0bbe673f992
DIO239 <agn15061997@yandex.ru>
2025-07-08 23:04:17 +0300

    edit

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,24 @@
 import { Component } from "@angular/core";
+import {Router} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
+  imports: [
+    MatButton
+  ],
   standalone: true
 })
 
 export class NotFoundComponent {
+  constructor(
+    private router: Router
+  ) {
+  }
 
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
